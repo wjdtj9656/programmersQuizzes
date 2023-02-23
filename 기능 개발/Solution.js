76d91e96fc08c3d8a3a9acddfb7a8a15a0e91022
wjdtj9656 <wjdtj9656@naver.com>
2023-02-23 11:16:21 +0900

    js

---
+++ 기능 개발/Solution.js
@@ -0,0 +1,16 @@
+function solution(progresses, speeds) {
+    var answer = [];
+    while(progresses.length){
+        for(let i=0; i<progresses.length; i++){
+            progresses[i] += speeds[i];
+        }
+        let cnt = 0;
+        while(progresses.length>0 && progresses[0] >= 100){
+            progresses.shift();
+            speeds.shift();
+            cnt++;
+        }
+        if(cnt > 0) answer.push(cnt);
+    }
+    return answer;
+}
